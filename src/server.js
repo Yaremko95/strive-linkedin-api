8d35435b2998e4c08c0e53e92bbc96c1eefef408
tetiana <tetianayaremko@gmail.com>
2020-07-20 13:25:04 +0200

    connects to mongo

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
+const mongoose = require('mongoose')
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,13 @@ app.use("/static", express.static(path.join(__dirname, "./public")));
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log("running on ", process.env.PORT);
-});
+mongoose
+    .connect(process.env.MONGOHOST, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    );
