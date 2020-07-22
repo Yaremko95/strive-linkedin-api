818fc2bfa889c48ff04b3a568b5a4c7bbd56c869
tetiana <tetianayaremko@gmail.com>
2020-07-22 16:13:03 +0200

    dir bug

--- src/utils/mkdir.js
+++ src/utils/mkdir.js
@@ -1,12 +1,27 @@
 const { join } = require("path");
 const mkdir = require("@root/mkdirp");
 const createDirectories = () => {
-  let dir = join(__dirname, "../", "public/posts");
-  mkdir(dir, function (error) {
+  let postsdir = join(__dirname, "../", "public/posts");
+  let profilesDir = join(__dirname, "../", "public/profiles");
+  let eduDir = join(__dirname, "../", "public/eduDir");
+
+  mkdir(postsdir, function (error) {
     if (error) {
       throw error;
     }
-    console.log(dir);
+    console.log(postsdir);
+  });
+  mkdir(profilesDir, function (error) {
+    if (error) {
+      throw error;
+    }
+    console.log(profilesDir);
+  });
+  mkdir(eduDir, function (error) {
+    if (error) {
+      throw error;
+    }
+    console.log(eduDir);
   });
 };
 
