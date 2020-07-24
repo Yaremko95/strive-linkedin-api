b0a7b4a4aa8f42f3d601fb783011041785c48cc1
tetiana <tetianayaremko@gmail.com>
2020-07-24 05:56:58 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ const commentsRouter = require("./routes/comments");
 const profilesRouter = require("./routes/profilesRoute");
 const educationRouter = require("./routes/educationRoute");
 const makeDirectory = require("./utils/mkdir");
-const cors_proxy = require("cors-anywhere");
 
 const allowedOrigins = [
   "http://localhost:3000",
@@ -22,6 +21,17 @@ makeDirectory();
 
 dotenv.config();
 const app = express();
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.set("twig options", {
   strict_variables: false,
@@ -47,16 +57,6 @@ app.use("/static", express.static(path.join(__dirname, "./public")));
 //   })
 // );
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
-//   next();
-// });
 app.use(express.json());
 app.use("/profile", profilesRouter);
 app.use("/profile", authorize, educationRouter);
@@ -70,13 +70,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(
-    cors_proxy
-      .createServer({
-        originWhitelist: [], // Allow all origins
-        requireHeader: ["origin", "x-requested-with"],
-        removeHeaders: ["cookie", "cookie2"],
-      })
-      .listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-      })
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    })
   );
