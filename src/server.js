e55e5874d791089bc051737a53a13143273dabca
tetiana <tetianayaremko@gmail.com>
2020-07-24 05:41:13 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -45,6 +45,16 @@ app.use(
     },
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(express.json());
 app.use("/profile", profilesRouter);
 app.use("/profile", authorize, educationRouter);
