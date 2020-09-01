2a4ec019af27571d2c202dc27a759554334f3fca
tetiana <tetianayaremko@gmail.com>
2020-09-01 17:43:15 +0200

    update

--- src/server.js
+++ src/server.js
@@ -54,7 +54,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     })
   );
