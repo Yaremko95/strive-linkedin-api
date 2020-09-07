55a233811a1dd2a367802763274589fcd86ac148
tetiana <tetianayaremko@gmail.com>
2020-09-07 14:41:14 +0200

    protects profilesRouter

--- src/routes/profilesRoute/index.js
+++ src/routes/profilesRoute/index.js
@@ -16,87 +16,101 @@ const { authenticate } = require("./helpers");
 
 const profilesRouter = express.Router();
 
-profilesRouter.get("/", authorization, async (req, res, next) => {
-  try {
-    const query = q2m(req.query);
-    const profiles = await ProfileSchema.find(
-      query.criteria,
-      query.options.fields
-    )
-      .skip(query.options.skip)
-      .limit(query.options.limit)
-      .sort(query.options.sort);
+profilesRouter.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      const query = q2m(req.query);
+      const profiles = await ProfileSchema.find(
+        query.criteria,
+        query.options.fields
+      )
+        .skip(query.options.skip)
+        .limit(query.options.limit)
+        .sort(query.options.sort);
 
-    res.send({
-      data: profiles.map((profile) => {
-        profile.password = "";
-        return profile;
-      }),
-      total: profiles.length,
-    });
-  } catch (error) {
-    next(error);
+      res.send({
+        data: profiles.map((profile) => {
+          profile.password = "";
+          profile.refresh_tokens = [];
+          return profile;
+        }),
+        total: profiles.length,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-profilesRouter.get("/:username", authorization, async (req, res, next) => {
-  try {
-    const username = req.params.username;
-    const profile = await ProfileSchema.findOne({
-      username: req.params.username,
-    });
-    console.log(profile);
-    profile.password = "";
-    res.send(profile);
-  } catch (error) {
-    console.log(error);
-    next("While reading profiles list a problem occurred!");
+profilesRouter.get(
+  "/:username",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      const username = req.params.username;
+      const profile = await ProfileSchema.findOne({
+        username: req.params.username,
+      });
+      console.log(profile);
+      profile.password = "";
+      profile.refresh_tokens = [];
+      res.send(profile);
+    } catch (error) {
+      console.log(error);
+      next("While reading profiles list a problem occurred!");
+    }
   }
-});
+);
 
-profilesRouter.get("/:username/pdf", authorization, async (req, res, next) => {
-  try {
-    await ProfileSchema.aggregate([
-      { $match: { username: req.params.username } },
-      {
-        $lookup: {
-          from: "experiences",
-          localField: "username",
-          foreignField: "username",
-          as: "experiences",
+profilesRouter.get(
+  "/:username/pdf",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      await ProfileSchema.aggregate([
+        { $match: { username: req.params.username } },
+        {
+          $lookup: {
+            from: "experiences",
+            localField: "username",
+            foreignField: "username",
+            as: "experiences",
+          },
         },
-      },
-      {
-        $lookup: {
-          from: "educations",
-          localField: "username",
-          foreignField: "username",
-          as: "educations",
+        {
+          $lookup: {
+            from: "educations",
+            localField: "username",
+            foreignField: "username",
+            as: "educations",
+          },
         },
-      },
-    ]).exec(async (err, user) => {
-      if (err) {
-        next(err);
-      }
-      user.password = "";
-      user[0].experiences.forEach((time) => {
-        time.startDate = new Date(time.startDate).getFullYear();
-        if (time.endDate) time.endDate = new Date(time.endDate).getFullYear();
-      });
-      user[0].educations.forEach((time) => {
-        time.startDate = new Date(time.startDate).getFullYear();
-        if (time.endDate) time.endDate = new Date(time.endDate).getFullYear();
-      });
+      ]).exec(async (err, user) => {
+        if (err) {
+          next(err);
+        }
+        user.password = "";
+        user[0].experiences.forEach((time) => {
+          time.startDate = new Date(time.startDate).getFullYear();
+          if (time.endDate) time.endDate = new Date(time.endDate).getFullYear();
+        });
+        user[0].educations.forEach((time) => {
+          time.startDate = new Date(time.startDate).getFullYear();
+          if (time.endDate) time.endDate = new Date(time.endDate).getFullYear();
+        });
 
-      const pdf = await getPdf(user[0], (stream) => {
-        res.set("Content-type", "application/pdf");
-        stream.pipe(res);
+        const pdf = await getPdf(user[0], (stream) => {
+          res.set("Content-type", "application/pdf");
+          stream.pipe(res);
+        });
       });
-    });
-  } catch (e) {
-    console.log(e);
+    } catch (e) {
+      console.log(e);
+    }
   }
-});
+);
 
 profilesRouter.post("/", async (req, res, next) => {
   try {
@@ -113,33 +127,43 @@ profilesRouter.post("/", async (req, res, next) => {
   }
 });
 
-profilesRouter.put("/:username", authorization, async (req, res, next) => {
-  try {
-    const user = basicAuth(req);
-    if (user.name !== req.params.username) res.status(403).send("unauthorized");
-    else {
-      const profile = await ProfileSchema.findOneAndUpdate(
-        { username: user.name },
-        {
-          ...req.body,
-          username: user.name,
-        },
-        { runValidators: true }
-      );
-      if (profile) {
-        res.send(profile);
-      } else {
-        const error = new Error(
-          `Profile with username ${req.params.username} not found`
+profilesRouter.put(
+  "/:username",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      // const user = basicAuth(req);
+
+      if (req.user.username !== req.params.username)
+        res.status(403).send("unauthorized");
+      else {
+        delete req.body.refresh_tokens;
+        delete req.body.facebookid;
+        delete req.body.googleid;
+        delete req.body._id;
+        const profile = await ProfileSchema.findOneAndUpdate(
+          { _id: req.user._id },
+          {
+            ...req.body,
+            _id: req.user._id,
+          },
+          { runValidators: true }
         );
-        error.httpStatusCode = 404;
-        next(error);
+        if (profile) {
+          res.send({ ...profile, refresh_tokens: [] });
+        } else {
+          const error = new Error(
+            `Profile with username ${req.params.username} not found`
+          );
+          error.httpStatusCode = 404;
+          next(error);
+        }
       }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 profilesRouter.post(
   "/logout",
@@ -249,12 +273,12 @@ profilesRouter.route("/refreshToken").post(async (req, res, next) => {
 
 profilesRouter
   .route("/:profileId")
-  .post(upload.single("profile"), authorization, async (req, res) => {
-    try {
-      const profile = await ProfileSchema.findById(req.params.profileId);
-      const user = basicAuth(req);
-      if (profile) {
-        if (profile.username === user.name) {
+  .post(
+    upload.single("profile"),
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      try {
+        if (req.user._id === req.params.profileId) {
           const [filename, extension] = req.file.mimetype.split("/");
           await fs.writeFile(
             join(profilesDirectory, `${req.params.profileId}.${extension}`),
@@ -276,13 +300,11 @@ profilesRouter
         } else {
           res.status(403).send("unauthorised");
         }
-      } else {
-        res.status(404).send("not found");
+      } catch (e) {
+        console.log(e);
+        res.status(500).send("bad request");
       }
-    } catch (e) {
-      console.log(e);
-      res.status(500).send("bad request");
     }
-  });
+  );
 
 module.exports = profilesRouter;
